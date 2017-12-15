0d27ea2c3d91e6fead8cb5a73ee71d787086e9ec
Lan Pham <lanpham312@gmail.com>
2017-12-15 12:53:22 +0000

    adding user accounts

--- app/bm.rb
+++ app/bm.rb
@@ -3,6 +3,8 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BM < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/links/new' do
     erb(:add)
@@ -33,6 +35,23 @@ class BM < Sinatra::Base
 
     link.save
     redirect '/links'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
   run! if app_file == $0
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -21,3 +21,11 @@ def add_sky_sports
   fill_in :tag, with: 'Sport'
   click_on "Add Bookmark"
 end
+
+def sign_up
+  visit '/users/new'
+  expect(page.status_code).to eq(200)
+  fill_in :email,    with: 'alice@example.com'
+  fill_in :password, with: 'oranges!'
+  click_button 'Sign up'
+end
